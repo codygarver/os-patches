e29ad3e685b7052abcc6424fe019c3090878a2cf
Corentin Noël <corentin@elementaryos.org>
2015-02-22 09:20:40 -0600

    Support Switchboard during Pantheon session

--- debian/gnome-control-center.sh
+++ debian/gnome-control-center.sh
@@ -3,6 +3,8 @@
 # Support legacy applications that still refer to gnome-control-center in Unity
 if [ "$XDG_CURRENT_DESKTOP" = "Unity" ] && [ -x /usr/bin/unity-control-center ]; then
   exec /usr/bin/unity-control-center $@
+elif [ "XDG_CURRENT_DESKTOP" = "Pantheon" ] && [ -x /usr/bin/switchboard ]; then
+  exec /usr/bin/switchboard $@
 else
   exec /usr/bin/gnome-control-center.real $@
 fi
